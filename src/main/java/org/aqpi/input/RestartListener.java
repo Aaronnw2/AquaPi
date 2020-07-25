66e0f8b68056ab0dc2d50306b3a1a9e8370ab07d
aaron <aaronnw2@gmail.com>
2020-07-25 10:16:47 -0700

    [Aaron] update restart listener to wait for 3 sec. to prevent restarts for temp power dips

--- src/main/java/org/aqpi/input/RestartListener.java
+++ src/main/java/org/aqpi/input/RestartListener.java
@@ -3,8 +3,10 @@ package org.aqpi.input;
 import static com.pi4j.io.gpio.PinPullResistance.PULL_UP;
 import static com.pi4j.io.gpio.PinState.LOW;
 import static com.pi4j.io.gpio.RaspiPin.GPIO_27;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import javax.annotation.PostConstruct;
 
@@ -14,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioPin;
 import com.pi4j.io.gpio.GpioPinDigitalInput;
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
@@ -36,13 +39,21 @@ public class RestartListener {
 		restartButton.addListener(new GpioPinListenerDigital() {
 			@Override
 			public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+				GpioPin pin = event.getPin();
 				LOG.info(String.format("PIN STATE CHANGE: %s, Edge: (%s, %s), Pin: %s, State: %s", event.getEventType(), event.getEdge().getName(),
-						event.getEdge().getValue(), event.getPin(), event.getState()));
-				if (event.getPin().getName().equals(RESTART_PIN_NAME) && event.getState().equals(LOW)) {
-					LOG.info("=================================== Restart Called, shutting down ===================================");
-					Runtime runtime = Runtime.getRuntime();
-					try { runtime.exec("sudo reboot"); }
-					catch (IOException e) { LOG.error("Error calling restart!", e); }
+						event.getEdge().getValue(), pin, event.getState()));
+				if (pin.getName().equals(RESTART_PIN_NAME) && event.getState().equals(LOW)) {
+					LOG.info("Detected input Low");
+					try {
+						SECONDS.sleep(2);
+						if (restartButton.isLow()) {
+							LOG.info("=================================== Restart Called, shutting down ===================================");
+							Runtime.getRuntime().exec("sudo reboot");
+						} else {
+							LOG.info("Possible voltage dip, skipping restart");
+						}
+					}
+					catch (IOException | InterruptedException e) { LOG.error("Error calling restart!", e); }
 				}
 			}
 		});
